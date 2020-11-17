8eda158581e03bf77fe53cabaa0a4d575a6153a0
simonacossai <cossaisimona@gmail.com>
2020-11-17 16:50:09 +0100

    DONE- making select work

--- src/components/LatestBooks/Books.jsx
+++ src/components/LatestBooks/Books.jsx
@@ -3,24 +3,46 @@ import './Books.css';
 import { Container, Row, Col, Card, Badge, Dropdown, DropdownButton } from "react-bootstrap";
 import horror from "../../data/horror.json";
 import romance from "../../data/romance.json";
+import history from "../../data/history.json";
+import scifi from '../../data/scifi.json';
 
 
 class Books extends React.Component {
+    state = {
+        book: horror,
+      };
+      
+      changeCategory = (event) => {
+     if(event.currentTarget.value==="horror"){
+        this.setState({ book: horror });
+     }else if(event.currentTarget.value==="romance"){
+        this.setState({ book: romance});
+     }else if(event.currentTarget.value==="history"){
+        this.setState({ book: history});
+     }else if(event.currentTarget.value==="sci-fi"){
+        this.setState({ book: scifi});
+
+     }
+};
+      
     render() {
       return (
     <div>
     <Container className=" justify-content-center mt-3 mb-4">
     <div class="d-flex align-items-center">
         <h1>Latest releases</h1> 
-        <DropdownButton id="dropdown-basic-button" className="ml-3 dropdown" title="Dropdown button">
-        <Dropdown.Item href="#/action-1" >Romance</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </DropdownButton>
+        <select id="dropdown-basic-button" className="ml-3 dropdown" onClick={this.changeCategory} >
+            <option >category</option>
+            <option value="romance">romance</option>
+            <option value="horror">horror</option>
+            <option value="history">history</option>
+            <option value="sci-fi">sci-fi</option>
+        </select>
     </div>
     <hr className="my-2" />
     <p>See the best books published recently</p>
     <Row className="justify-content-start mt-3">     
-    {horror.map((book) => (
+    {this.state.book.map((book) => (
         <Col className="mt-3" key={book.asin} >
         <Card style={{ width: '18rem' }} className="card">
             <Card.Img variant="top" src={book.img} className="books-image"/>
